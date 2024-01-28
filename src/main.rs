0f6f9eb2f98167f584fac79e2b0e339643d8d0ea
Vhyrro <vhyrro@gmail.com>
2024-01-28 20:21:28 +0100

    refactor: refactor codebase to make more sense

--- src/main.rs
+++ src/main.rs
@@ -9,53 +9,80 @@ struct NorgFmt {
     file: PathBuf,
 }
 
-pub fn parse_heading(node: &Node, source: &mut String) -> Result<()> {
-    let depth = node
-        .named_child(0)
-        .ok_or(eyre!("heading has no stars"))?
-        .utf8_text(source.as_bytes())?;
+fn rest(children: Vec<NorgNode>, from: Option<usize>) -> String {
+    children
+        .into_iter()
+        .skip(from.unwrap_or(0))
+        .fold(String::default(), |str, val| str + &val.content)
+}
 
-    let title = node.named_child(1).ok_or(eyre!("heading has no title"))?;
-
-    let result = format!(
+pub fn parse_heading(_node: &Node, children: Vec<NorgNode>, _: &String) -> Result<String> {
+    let heading_header = format!(
         "{} {}",
-        "*".repeat(depth.len()),
-        title.utf8_text(source.as_bytes())?
+        children
+            .get(0)
+            .ok_or(eyre!("heading has no stars"))?
+            .content,
+        children
+            .get(1)
+            .ok_or(eyre!("heading has no title"))?
+            .content,
     );
 
-    source.replace_range(node.start_byte()..title.end_byte(), &result);
+    Ok(heading_header + &rest(children, Some(2)))
+}
 
-    Ok(())
+pub fn parse_stars(node: &Node, _: Vec<NorgNode>, source: &String) -> Result<String> {
+    Ok(node.utf8_text(source.as_bytes())?.trim().to_string())
+}
+
+pub fn parse_title(node: &Node, _: Vec<NorgNode>, source: &String) -> Result<String> {
+    Ok(node.utf8_text(source.as_bytes())?.trim().to_string())
+}
+
+pub struct NorgNode {
+    // field: Option<String>,
+    pub kind: String,
+    pub content: String,
+}
+
+pub fn parse(node: &Node, source: &String) -> Result<String> {
+    // println!("'{}'", node.kind());
+    let mut children = vec![];
+
+    for child in node.children(&mut node.walk()) {
+        let content = parse(&child, source)?;
+
+        children.push(NorgNode {
+            kind: child.kind().to_string(),
+            content,
+        })
+    }
+
+    let ret = match node.kind() {
+        "heading" => parse_heading(node, children, source)?,
+        "heading_stars" => parse_stars(node, children, source)?,
+        "title" => parse_title(node, children, source)?,
+        "document" => rest(children, None),
+        _ => node.utf8_text(source.as_bytes())?.to_string(),
+    };
+
+    Ok(ret)
 }
 
 fn main() -> Result<()> {
     let cli = NorgFmt::parse();
 
     let file = cli.file;
-    let mut content = String::from_utf8(std::fs::read(file)?)?;
+    let content = String::from_utf8(std::fs::read(file)?)?;
 
     let mut parser = Parser::new();
     parser.set_language(tree_sitter_norg::language())?;
 
     let tree = parser.parse(&content, None).unwrap();
 
-    let mut cursor = tree.walk();
-
-    let mut node = cursor.node();
-
-    while cursor.goto_next_sibling() || cursor.goto_first_child() {
-        match node.kind() {
-            "heading" => parse_heading(&node, &mut content),
-            _ => {
-                node = cursor.node();
-                continue;
-            }
-        }?;
-
-        node = cursor.node();
-    }
-
-    println!("{}", content);
+    println!("{}", tree.root_node().to_sexp());
+    println!("{}", parse(&tree.root_node(), &content)?);
 
     Ok(())
 }
